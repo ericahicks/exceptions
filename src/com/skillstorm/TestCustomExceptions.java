5c6c5fea8657c6f7503ecf4780b3e27095adde36
Daun Kim <101144992+daunkim18@users.noreply.github.com>
2023-05-24 14:35:39 -0500

    first commit

--- src/com/skillstorm/TestCustomExceptions.java
+++ src/com/skillstorm/TestCustomExceptions.java
@@ -3,34 +3,52 @@ package com.skillstorm;
 public class TestCustomExceptions {
 
 	public static void main(String[] args) throws Exception {
-		// TODO Add your code without overwriting anyone else's
-		// submit the .java file at the time you merged
-		// Step 1: Create a custom exception (inherit from whatever you want)
-		// Step 2: Add a catch clause (and any other code your need)
-		try {
-			doStuff();
-		} catch (EricasException2 e) {
-			System.out.println(e);
-		} catch (EricasException3 e) {
-			System.out.println(e);
-		} catch (EricasException e) {
-			System.out.println(e);
-		} catch (Exception e) {
-			System.out.println(e);
+			// TODO Add your code without overwriting anyone else's
+			// submit the .java file at the time you merged
+			// Step 1: Create a custom exception (inherit from whatever you want)
+			// Step 2: Add a catch clause (and any other code your need)
+			try {
+				doStuff();
+			} catch (EricasException2 e) {
+				System.out.println(e);
+			} catch (EricasException3 e) {
+				System.out.println(e);
+			} catch (EricasException e) {
+				System.out.println(e);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			
+			try {
+				doStuff();
+			} catch (DaunsException2 e) {
+				System.out.println(e);
+			} catch (DaunsException3 e) {
+				System.out.println(e);
+			} catch (DaunsException e) {
+				System.out.println(e);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			
+
 		}
 		
+		public static void doStuff() throws EricasException, DaunsException {
+			
+		}
 
 	}
+
+	// Erica's code
+	class EricasException extends Exception {}
+	class EricasException2 extends EricasException {}
+	class EricasException3 extends EricasException {}
+
+	// Blake's code
 	
-	public static void doStuff() throws EricasException {
-		
-	}
+	//Daun's code 
+	class DaunsException extends Exception {}
+	class DaunsException2 extends DaunsException {}
+	class DaunsException3 extends DaunsException {}
 
-}
-
-// Erica's code
-class EricasException extends Exception {}
-class EricasException2 extends EricasException {}
-class EricasException3 extends EricasException {}
-
-// Blake's code
