c3a223d741668168b6e238e11cbba1c248202b5f
TenzinWangden <129078538+TenzinWangden@users.noreply.github.com>
2023-05-24 14:35:00 -0400

    Tenzin's first commit

---
+++ src/com/skillstorm/TestCustomExceptions.java
@@ -0,0 +1,41 @@
+package com.skillstorm;
+
+public class TestCustomExceptions {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Add your code without overwriting anyone else's
+		// submit the .java file at the time you merged
+		// Step 1: Create a custom exception (inherit from whatever you want)
+		// Step 2: Add a catch clause (and any other code your need)
+		try {
+			doStuff();
+		} catch (EricasException2 e) {
+			System.out.println(e);
+		} catch (EricasException3 e) {
+			System.out.println(e);
+		} catch (EricasException e) {
+			System.out.println(e);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+
+	}
+	
+	public static void doStuff() throws EricasException {
+		
+	}
+
+}
+
+// Erica's code
+class EricasException extends Exception {}
+class EricasException2 extends EricasException {}
+class EricasException3 extends EricasException {}
+
+// Blake's code
+
+// Tenzin's code
+class TenzinsException extends Exception {}
+class TenzinsException2 extends EricasException {}
+class TenzinsException3 extends EricasException {}
